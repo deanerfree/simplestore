53ea0a7e4b6313f771be7515f32c77c0ad9f0b9a
Kurt Ball <kurtisdball@hotmail.com>
2021-06-16 14:21:04 -0600

    create a user, added userSchema

---
+++ server/model/user.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  passwordHash: { type: String, required: true },
+  street: { type: String, default: '' },
+  apartment: { type: String, default: '' },
+  city: { type: String, default: '' },
+  zip: {type: Number, default: 0}
+  country: { type: String, required: true },
+  phone: { type: Number, default: 0 },
+  isAdmin: { type: Boolean, required: false },
+})
+
+//This bit of code will set _id to id
+userSchema.virtual('id').get(function () {
+  return this._id.toHexString()
+})
+
+userSchema.set('toJSON', {
+  virtuals: true,
+})
+
+exports.User = mongoose.model('User', userSchema)
+exports.userSchema = userSchema
